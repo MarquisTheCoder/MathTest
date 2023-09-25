76b7d0c9816203e785582ed17a8eee00df3f08b3
MarquisTheCoder <deshawn.m.williams01@gmail.com>
2023-09-24 20:27:23 -0500

    MarquisTheCoder:  Sun Sep 24 20:27:23 CDT 2023

---
+++ src/test/java/com/interviewtest/math/MyRestControllerTest.java
@@ -0,0 +1,31 @@
+import static org.hamcrest.Matchers.matchesPattern;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class MyRestControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void testEndpointReturnsAlphanumericValue() throws Exception {
+        String endpointUrl = "/api/resource"; // Replace with your actual endpoint URL
+        String requestBody = "{\"key\": \"value\"}"; // Replace with your POST request body
+
+        mockMvc.perform(post(endpointUrl)
+                .content(requestBody)
+                .contentType(MediaType.APPLICATION_JSON))
+               .andExpect(status().isOk())
+               .andExpect(content().string(matchesPattern("^[a-zA-Z0-9]+$"))); // Regex pattern for alphanumeric
+    }
+}
